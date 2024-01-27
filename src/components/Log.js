048825f9ff109673a09b7be40847e0a93cea1768
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2024-01-27 18:49:56 +0200

    Log delete UI

--- src/components/Log.js
+++ src/components/Log.js
@@ -7,7 +7,9 @@ export default function Log(props) {
   return (
     <React.Fragment>
     <div className='log-general'>
-    <div className='logged'> <h4>{log.work}</h4> </div> <div className='log-hours'><h4> {log.userName}  {log.hours} on {log.logDate}</h4></div> 
+    <div className='logged'> <h4>{log.work}</h4> </div>
+    <div className='log-hours'><h4> {log.userName}  {log.hours} on {log.logDate}</h4><div className='log-delete' onClick={() => console.log("Log delete!")}><h4>X</h4></div> </div> 
+    
     </div>
     </React.Fragment>
   )
