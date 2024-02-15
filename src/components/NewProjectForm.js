2e1a3d61582388edcbc5486a2df1e88745afbe7d
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2024-02-15 18:19:12 +0200

    Styling New Project Form

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export default function NewProjectForm() {
+
+    function handleNewProjectSubmission(event) {
+        event.preventDefault();
+
+    }
     return (
         <React.Fragment>
-            <h3>New Project Form</h3>
+            <h2 className='new-project-h2'>New Project Form</h2>
+            <form className='new-project-form' onSubmit={handleNewProjectSubmission}>
+             <input
+             name='projectName'
+             type='text'
+             placeholder='Enter project name'
+             />
+              <input
+             name='projectDeadLine'
+             type='date'
+             placeholder='Enter project deadline'
+             />
+             <button type='submit' className='add-project-btn'>Add project</button>
+            </form>
         </React.Fragment>
     );
 }
